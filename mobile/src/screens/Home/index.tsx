227a52dfc686c0cba1e6d91ec422da997164d307
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2022-10-27 23:19:03 -0300

    feature - landing page mobile finalizada

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,30 @@
+import { View, Image, FlatList } from "react-native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Header } from "../../components/Header";
+import { GameCard } from "../../components/GameCard";
+import { styles } from "./styles";
+import { GAMES } from "../../utils/games";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Header
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          <GameCard data={item} />;
+        }}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+    </View>
+  );
+}
