d3b6821351b86d6aed95071a159be9574786da77
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2022-12-23 14:08:59 -0300

    feature - header da pagina de detalhes do jogo

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,20 +1,47 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Background } from "../../components/Background";
-import { useRoute } from "@react-navigation/native";
-
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { styles } from "./styles";
 import { GameCardProps } from "../../components/GameCard/types";
+import { TouchableOpacity, View, Image } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { THEME } from "../../theme";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Header } from "../../components/Header";
 
 export function Game() {
+
+  const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameCardProps;
 
-  console.log(game);
+  const handleGoBack = () => {
+    navigation.goBack();
+  }
 
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => handleGoBack()}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image source={logoImg} style={styles.logo} />
+          {/* Utilizada para centralizar o a logo */}
+          <View style={styles.invisibleIcon} />
+        </View>
+
+        <Header
+          title={game.title}
+          subtitle={"Conecte-se e comece a jogar!"}
+        />
+      </SafeAreaView>
     </Background>
   );
 }
